cedb5bd18998a2450f0963a0552586f4bfce8242
Layachi <16508827+AhLay@users.noreply.github.com>
2018-04-03 23:19:35 +0200

    DI initial checkin

---
+++ Lessons.DependencyInjection/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.DependencyInjection
+{
+    public class Rectangle : IShape
+    {
+        public void Display()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
