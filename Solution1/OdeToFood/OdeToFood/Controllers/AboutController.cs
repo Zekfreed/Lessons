3da062f98434cc1388644dbebe7ebdfc320f8c63
Zekfreed <zekfreed@gmail.com>
2018-04-05 23:37:26 +0100

    Up to date!

---
+++ Solution1/OdeToFood/OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    [Route("company/[controller]/[action]")]
+    public class AboutController
+    {
+         
+        public string Phone()
+        {
+            return "0+666+57+45+20";
+        }
+         
+        public string Address()
+        {
+            return "MAROC";
+        }
+    }
+}
---
+++ Solution1/OdeToFood/OdeToFood/Services/InMemoryRestaurant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class InMemoryRestaurant : IRestaurantData
+    {
+        public InMemoryRestaurant()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant {Id=1 , Name="Scott´s Pizza Place"},
+                new Restaurant {Id=2 , Name="Tersiguels"},
+                new Restaurant {Id=3 , Name="King's Contrivance"}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants.OrderBy(r => r.Name);
+        }
+
+        public Restaurant Get(int id)
+        {
+            return _restaurants.FirstOrDefault(r => r.Id == id);
+        }
+
+        List<Restaurant> _restaurants;
+    }
+}
